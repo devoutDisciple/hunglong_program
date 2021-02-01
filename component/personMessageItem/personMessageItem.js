18cf9ec58d74e0346a527e51f811563e6c38c60c
张振 <1094705507@qq.com>
2021-02-01 18:15:57 +0800

    完成个人信息页面

--- component/personMessageItem/personMessageItem.js
+++ component/personMessageItem/personMessageItem.js
@@ -1,23 +1,49 @@
 // component/personMessageItem/personMessageItem.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
+		label: {
+			type: String,
+			value: '',
+		},
+		value: {
+			type: String,
+			value: '',
+		},
+		key: {
+			type: String,
+			value: '',
+		},
+		type: {
+			type: String,
+			value: '',
+		},
+	},
 
-  },
+	lifetimes: {
+		attached: function () {
+			// 在组件实例进入页面节点树时执行
+			// console.log(this.properties, 888);
+		},
+		detached: function () {
+			// 在组件实例被从页面节点树移除时执行
+		},
+	},
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+	/**
+	 * 组件的初始数据
+	 */
+	data: {},
 
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
-})
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {
+		click: function (e) {
+			const { key } = e.currentTarget.dataset;
+			this.triggerEvent('click', key);
+		},
+	},
+});
