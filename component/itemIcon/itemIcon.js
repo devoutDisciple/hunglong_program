986e9985c2d6fed419c7aaadb72f7a7e23b2067f
张振 <1094705507@qq.com>
2021-03-01 22:58:44 +0800

    完成登录功能

---
+++ component/itemIcon/itemIcon.js
@@ -0,0 +1,26 @@
+// component/itemIcon/itemIcon.js
+Component({
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
+		label: {
+			type: String,
+			value: '',
+		},
+		key: {
+			type: String,
+			value: '',
+		},
+	},
+
+	/**
+	 * 组件的初始数据
+	 */
+	data: {},
+
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {},
+});
---
+++ pages/selBackImg/selBackImg.js
@@ -0,0 +1,34 @@
+// pages/selBackImg/selBackImg.js
+Page({
+	/**
+	 * 页面的初始数据
+	 */
+	data: {},
+
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {},
+
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {},
+
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {},
+
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {},
+
+	/**
+	 * 点击预览
+	 */
+	onPrivewImg: function () {
+		console.log(123);
+	},
+});
