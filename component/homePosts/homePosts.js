c02e07fa6c45267884a586be5a9930d7538a3c04
张振 <1094705507@qq.com>
2021-03-05 00:02:14 +0800

    首页博客

--- component/homePosts/homePosts.js
+++ component/homePosts/homePosts.js
@@ -8,10 +8,41 @@ Component({
 	/**
 	 * 组件的初始数据
 	 */
-	data: {},
+	data: {
+		imgList: [
+			// {
+			// 	url: '/asserts/temp/1.jpg',
+			// 	width: 600,
+			// 	height: 500,
+			// },
+			// {
+			// 	url: '/asserts/temp/2.jpg',
+			// 	width: 600,
+			// 	height: 500,
+			// },
+			// {
+			// 	url: '/asserts/temp/3.jpg',
+			// 	width: 600,
+			// 	height: 500,
+			// },
+			// {
+			// 	url: '/asserts/temp/88.png',
+			// 	width: 600,
+			// 	height: 700,
+			// },
+			{
+				url: '/asserts/temp/99.png',
+				width: 600,
+				height: 400,
+			},
+		],
+	},
 
 	/**
 	 * 组件的方法列表
 	 */
 	methods: {},
+
+	// 组件生命周期函数-在组件实例进入页面节点树时执行)
+	attached: function () {},
 });
---
+++ component/postImg/postImg.js
@@ -0,0 +1,48 @@
+// component/postImg/postImg.js
+Component({
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
+		imgList: {
+			type: Array,
+			value: [],
+		},
+	},
+
+	/**
+	 * 组件的初始数据
+	 */
+	data: {},
+
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {},
+
+	/**
+	 * 生命周期
+	 */
+	lifetimes: {
+		attached: function () {
+			const { imgList } = this.data;
+			if (Array.isArray(imgList) && imgList.length > 2) {
+				const len = imgList.length;
+				const remain = len % 3;
+				if (remain === 1) {
+					const newImgList = imgList.concat([{ empty: true }, { empty: true }]);
+					return this.setData({ imgList: newImgList, imgListLen: imgList.length });
+				}
+				if (remain === 2) {
+					const newImgList = imgList.concat([{ empty: true }]);
+					return this.setData({ imgList: newImgList, imgListLen: imgList.length });
+				}
+			}
+			console.log(imgList, imgList.length, 111);
+			this.setData({ imgListLen: imgList.length });
+		},
+		detached: function () {
+			// 在组件实例被从页面节点树移除时执行
+		},
+	},
+});
