16d76ca7e6f4c624847457b34ce6b3c9ba0bb85c
张振 <1094705507@qq.com>
2021-04-28 23:52:13 +0800

    修改g个人中心

---
+++ pages/person/person.js
@@ -0,0 +1,74 @@
+import { getDeviceInfo } from '../../utils/util';
+
+Page({
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		navHeight: '40px',
+		statusBarHeight: '20px',
+		backIconHeight: '20px',
+		backIconMarginTop: '10px',
+		bgData: {
+			url: '/asserts/temp/16.png',
+			width: 300,
+			height: 100,
+		},
+	},
+
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function () {
+		this.getDeviceData();
+	},
+
+	// 获取设备信息
+	getDeviceData: function () {
+		// 获取设备信息
+		getDeviceInfo().then((res) => {
+			console.log(res);
+			this.setData({
+				navHeight: `${res.navHeight}px`,
+				statusBarHeight: `${res.statusBarHeight}px`,
+				backIconHeight: `${res.navHeight / 2}px`,
+				backIconMarginTop: `${res.navHeight / 4}px`,
+			});
+		});
+	},
+
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {},
+
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {},
+
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {},
+
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {},
+
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {},
+
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {},
+
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {},
+});
